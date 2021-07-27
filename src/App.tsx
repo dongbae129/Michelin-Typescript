d69e6158a0315c474ef464ea6da1fa43387dc43c
Dong <dongbae129@naver.com>
2021-07-27 16:21:49 +0900

    typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+import Upload from "./pages/Upload";
+import Restaurant from "./pages/Restaurant";
+import aa from "./pages/aa";
+import DetailType from "./components/DetailType";
+import "./css/app.css";
 
-function App() {
+function App(): JSX.Element {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <BrowserRouter>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          height: "9vh",
+          padding: "10px",
+        }}
+      >
+        <div style={{ width: "50px", height: "50px", zIndex: 1 }}>
+          <Link
+            to="/"
+            style={{
+              marginLeft: "10px",
+            }}
+          >
+            <img
+              src="/images/mainlogo.png"
+              alt="logo"
+              style={{ width: "100%", height: "100%", borderRadius: "50%" }}
+            />
+          </Link>
+        </div>
+        <div
+          style={{
+            marginLeft: "40px",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "50px",
+            padding: "10px",
+            borderRadius: "10px",
+            background: "#e3fafc",
+            zIndex: 1,
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          {" "}
+          <Link
+            style={{
+              textDecoration: "none",
+              color: "#495057",
+              fontSize: "30px",
+              verticalAlign: "middle",
+              fontFamily: "'Nanum Pen Script', cursive",
+            }}
+            to="/upload"
+          >
+            Upload
+          </Link>
+        </div>
+      </div>
+
+      <Route path="/" component={Home} exact />
+      <Route path="/upload" component={Upload} />
+      <Switch>
+        <Route path="/restaurant/detailinfo" component={DetailType} />
+        <Route path="/restaurant/search" component={aa} />
+        <Route path="/restaurant" component={Restaurant} />
+      </Switch>
+      <Route path="/food/:type" component={DetailType} />
+    </BrowserRouter>
   );
 }
 
